5352f3b430a38b03c688936387cb1cad562242d1
dannyganza <kingdanny@Dannys-Air.hitronhub.home>
2023-03-23 17:42:53 -0600

    added

---
+++ js/app.js
@@ -0,0 +1,6 @@
+let get_books_p_tag = document.querySelector(`p`)
+let get_p_tag_by_class= document.querySelectorAll(`.hii`)
+let id_finder = document.querySelector(`h1`);
+
+
+let use_id_finder= document.getElementById(`hello`)
